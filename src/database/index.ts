6e7d0563f94e6cf72504715c025f0a6bc42aff0c
Halex Viotto Gomes <halexx002@gmail.com>
2021-06-22 16:43:01 -0400

    Class 2 finished

---
+++ src/database/index.ts
@@ -0,0 +1,5 @@
+import { createConnection } from "typeorm";
+
+createConnection();
+
+//Cria a conexão com o banco de dados
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
+import "reflect-metadata";
 import express from "express";
+
+//Se for o index não precisa colocar no caminho, importa o index automático de uma pasta
+import "./database";
+
+import { router } from "./routes";
 
 const app = express();
 
-app.get("/test", (req, res) => {
-  return res.send("Olá NLW!");
-});
+app.use(express.json());
+
+app.use(router);
 
 app.listen(3000, () => console.log("Server is running"));
